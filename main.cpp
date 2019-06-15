80aa6d672cc6f31674c4e12c11eb09cdf86bdf4f
Grzesiek <grzegorz.jjarz@gmail.com>
2019-06-15 15:44:15 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/*  Implenetacja algorytmu Apriopri
+
+    Założenia
+    
+    input - dane w postaci vectorow typu T
+    output - reguły w postaci vectora par.
+    kazda para K reprezentuje regułę(zaleznosc)
+    K == < vec<T>, vec<T> >
+ */
+
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
